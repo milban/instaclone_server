c9215c1b0cda36444efe65551332117b0ec3321c
JunHyeock Park <wnsgur6311@gmail.com>
2019-09-27 17:37:52 +0900

    SendMail with Nodemailer&SendGrid

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,39 @@
+import nodemailer from "nodemailer"
+import sgTransport from "nodemailer-sendgrid-transport"
 import { adjectives, nouns } from "./words"
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config({ path: path.resolve(__dirname, ".env") })
 
 export const generateSecret = () => {
   const adjectivesRandomNumber = Math.floor(Math.random() * (adjectives.length - 1))
   const nounsRandomNumber = Math.floor(Math.random() * (nouns.length - 1))
   return `${adjectives[adjectivesRandomNumber]} ${nouns[nounsRandomNumber]}`
+}
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  }
+  const client = nodemailer.createTransport(sgTransport(options))
+  return client.sendMail(email, (err, info) => {
+    if (err) {
+      console.log(error)
+      return
+    }
+    console.log(`Message sent: ${info.response}`)
+  })
+}
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "milban@milbangram.milban",
+    to: address,
+    subject: "Login Secret for Milbangram ⭐️",
+    html: `Hello! Your login secret is ${secret} <br/> Copy & Paste on the web to log in.`
+  }
+  sendMail(email)
 }
