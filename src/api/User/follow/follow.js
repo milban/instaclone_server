267bf772a3cb576da06afc28b225753d74fe3708
JunHyeock Park <wnsgur6311@gmail.com>
2019-09-30 15:43:16 +0900

    Follow Unfollow

---
+++ src/api/User/follow/follow.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../generated/prisma-client"
+import { isAuthenticated } from "../../../middlewares"
+
+export default {
+  Mutation: {
+    follow: async (_, args, { request }) => {
+      isAuthenticated(request)
+      const { user } = request
+      const { id } = args
+      try {
+        await prisma.updateUser({ where: { id: user.id }, data: { followings: { connect: { id } } } })
+        return true
+      } catch (error) {
+        return false
+      }
+    }
+  }
+}
